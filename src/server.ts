3312cc83399f35e4efdf0d6c4423ab3db8975fde
carlosd <carlosd@clubegazetadopovo.com.br>
2024-06-25 11:50:37 -0300

    feat: config eslint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
 // Setar o host, para evitar problemas quando front end tentar acessar as rotas
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then(() => console.log('🚀 HTTP Server Running'))
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => console.log('🚀 HTTP Server Running'))
