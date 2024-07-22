21115e85d00acbe54d055cb60072982e34802127
carlosd <carlosd@clubegazetadopovo.com.br>
2024-07-22 09:58:56 -0300

    feat: Prisma integration, estructure use cases and implementa inversion dependecy

--- README.md
+++ README.md
@@ -30,3 +30,19 @@ GymPass style app.
 - [ ] Os dados da aplicação precisam estar persistidos em um banco PostgresSQL
 - [ ] Todas as listas de dadso precisam estar paginadas com 20 itens por página;
 - [ ] O usuário deve ser identificado por um JWT (JSON Web Token);
+
+
+
+
+## Comandos Docker
+
+- docker compose up -d -> Iniciar o container
+- docker compose down -> Parar deletar o banco
+- docker compose stop -> Parar o container
+- docker run name api-solid-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGRESQL_DATABASE=apisolid -p 5432:5432 bitnami/postgresql -> Para criar o banco
+
+
+## Comandos Prisma
+
+- npx prisma migrate dev: Para criar ou atualizar o banco
+- npx prisma studio: Para iniciar interface do banco
---
+++ src/user-cases/register.ts
@@ -0,0 +1,34 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUserCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+// SOLID
+
+// D - Dependency Inversion Principle
+
+export class RegisterUseCase {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: UsersRepository) { }
+
+  async execute({ name, email, password }: RegisterUserCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
